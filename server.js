fcbcf4e131da18fb73bd6df475581aa282e1bf0a
LipatovAV <lipa-andrej@yandex.ru>
2019-01-13 14:38:43 +0300

    fisrst scripts

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const http= require('http');
+const fs = require('fs');
+const server = http.createServer(function (request, response) {
+    if (request.url === '/main.css'){
+        const text = fs.readFileSync('main.css', 'utf-8');
+        response.end(text);
+    } else {
+        const text = fs.readFileSync('index.html', 'utf-8');
+        response.end(text);
+    }
+});
+server.listen(8000);
+console.log('server start');
