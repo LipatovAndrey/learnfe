9b72c8bc89624f91446dbf50cb4dc0c5e7c2bc3a
LipatovAV <lipa-andrej@yandex.ru>
2019-01-15 20:28:42 +0300

    add buttons

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const sections = [
+    ['login','Окно логина'],
+    ['profile','Профиль'],
+    ['about','Оо']
+];
+const nav = document.getElementById('navigation');
+for (let section of sections) {
+    const button = document.createElement('input');
+    button.setAttribute('type', 'button');
+    button.setAttribute('data-section', section[0]);
+    button.value=section[1];
+    nav.appendChild(button)
+}
